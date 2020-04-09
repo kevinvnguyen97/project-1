08a595f1c123cc3d6dd0d41d6e6bae5d289399d7
Maura Parker <mauraparker02@gmail.com>
2020-04-09 10:49:21 -0400

    trying to merge the meme button

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 var cors = "https://cors-anywhere.herokuapp.com/";
 var gameQueryURL = "https://api-v3.igdb.com/games/?search=";
+var gameImageURL = "https://images.igdb.com/igdb/image/upload/t_logo_med/";
+var imageId;
 var gameName = "";
 
 //queryURL = apiURL + key;
 
 $('.btn').on("click", function(event) {
-    event.preventDefault();
+    //event.preventDefault();
     gameName = $("#search-input").val().trim();
     // console.log(gameName);
 
@@ -16,11 +18,14 @@ $('.btn').on("click", function(event) {
         headers: {
             "user-key": "723e84ca4cb5bdd0d87cfb9d7e245b76"
         },
-        method: "GET"
+        method: "POST"
     }).then(function(gameResponse) {
         //console.log(gameResponse);
-        //console.log(gameResponse[0].name);
-        //console.log(gameResponse[0].summary);
+        localStorage.clear();
+        localStorage.setItem("info", JSON.stringify(gameResponse));
+        var a = localStorage.getItem("info");
+        a = JSON.parse(a);
+        console.log("Saved name " + a[0].name);
 
         //console.log("Description: " + response.)
         //var gameText = $("#game").text(JSON.stringify(response));
